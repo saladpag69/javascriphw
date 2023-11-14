75a192e65b86d1f9fc76afd94b884860cbfc3b1b
saladpag69 <saladpag@gmail.com>
2023-11-14 20:35:44 +0700

    lasthomework

---
+++ homework4/myFirstJavascript.js
@@ -0,0 +1,13 @@
+let arr2 =[1,2,3,4,5,6,7,8,9,10]
+debugger
+function myFunc(){
+    for(let i=0;i<2;i++){
+        console.log("Helllow")
+    }
+}
+
+for(let i=0;i<2;i++){
+    debugger
+    console.log(arr2[i])
+
+}
